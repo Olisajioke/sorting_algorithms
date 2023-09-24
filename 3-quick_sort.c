58cbb4e4c3d7aa33610ef3ff9642b9f458b0472b
olisajioke <chijioke914@mail.com>
2023-09-24 14:44:44 -0700

    Task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 void swap(int *a, int *b);
-int lomuto_partition(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 
 /**
@@ -31,7 +31,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int pivot_index = lomuto_partition(array, low, high, size);
+		int pivot_index = lomuto_partition(array, low, high);
 
 		print_array(array, size);
 
@@ -46,16 +46,16 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  * @array: The array to be partitioned
  * @low: The lowest index of the partition
  * @high: The highest index of the partition
- * @size: Number of elements in the array
  *
  * Return: The index of the pivot element after partition
  */
-int lomuto_partition(int *array, int low, int high, size_t size)
+int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
 	int i = low - 1;
+	int j;
 
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
